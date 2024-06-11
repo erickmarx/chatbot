849d81dae4cfafd4420c90411f6adf4a7afffc8c
erickmarx <erickmarx5@gmail.com>
2024-06-10 22:05:37 -0300

    remove unnecessary code

--- src/shared/adapter/langchain/langchain.provider.ts
+++ src/shared/adapter/langchain/langchain.provider.ts
@@ -47,28 +47,4 @@ export class LangchainProvider {
       input_documents: [document],
     });
   }
-
-  async contextualize(context: string, content: string) {
-    const contextualizeQSystemPrompt = `Given a chat history and the latest user question
-    which might reference context in the chat history, formulate a standalone question
-    which can be understood without the chat history. Do NOT answer the question,
-    just reformulate it if needed and otherwise return it as is.`;
-
-    const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
-      new SystemMessage(contextualizeQSystemPrompt),
-      new MessagesPlaceholder('chat_history'),
-      ['human', '{question}'],
-    ]);
-    const contextualizeQChain = contextualizeQPrompt
-      .pipe(this.openAIProvider.chatOpenAI)
-      .pipe(new StringOutputParser());
-
-    await contextualizeQChain.invoke({
-      chat_history: [
-        new HumanMessage('What does LLM stand for?'),
-        new AIMessage('Large language model'),
-      ],
-      question: 'What is meant by large',
-    });
-  }
 }
