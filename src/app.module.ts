b795794360fdd3a32129a194486063c30decab8f
erickmarx <erickmarx5@gmail.com>
2024-06-18 20:57:32 -0300

    fix: build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { AdapterModule } from './shared/adapter/adapter.module';
 
 @Module({
   imports: [AdapterModule, ChatModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
